0e8c9e1fbb8ab10f6ca9b73ac26cf324220fb26f
gianluv00 <gianluv00@mi.fu-berlin.de>
2022-06-05 20:01:02 +0200

    Added simple UI for name and health of the selected agent

---
+++ Assets/Scrips/UI/UIGeneralAgentInfo.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using Priority_Queue;
+using Scrips.EventManager;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class UIGeneralAgentInfo : MonoBehaviour {
+    
+    private Agent _selectedAgent;
+
+    private TMP_Text _agentNameText;
+    private TMP_Text _agentHealthText;
+    
+    // Start is called before the first frame update
+    void Start() {
+        AgentEventManager.current.OnAgentSelected += OnAgentSelected;
+        AgentEventManager.current.OnAgentDeselected += OnAgentDeselected;
+
+        TimeEventManager.current.OnTick += OnTick;
+
+        _agentNameText = transform.GetChild(0).GetComponent<TMP_Text>();
+        _agentHealthText = transform.GetChild(1).GetComponent<TMP_Text>();
+
+        _agentNameText.enabled = false;
+        _agentHealthText.enabled = false;
+    }
+
+    private void OnAgentSelected(Agent agent) {
+        _selectedAgent = agent;
+
+        _agentNameText.enabled = true;
+        _agentHealthText.enabled = true;
+
+        _agentNameText.text = agent.name;
+        SetHP();
+    }
+
+    private void OnAgentDeselected() {
+        _selectedAgent = null;
+        
+        _agentNameText.enabled = false;
+        _agentHealthText.enabled = false;
+    }
+
+    private bool IsAgentSelected() {
+        return _selectedAgent != null;
+    }
+
+    private void OnTick(int timeStep) {
+        UpdateGUI();
+    }
+
+    private void SetHP() {
+        _agentHealthText.text = _selectedAgent.GetHealth() + "HP";
+    }
+    
+    private void UpdateGUI() {
+        if (!IsAgentSelected()) return;
+        SetHP();
+    }
+}
