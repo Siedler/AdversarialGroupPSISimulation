7bbb6f5826fb5525d45dcdbef72c9984d3197832
gianluv00 <gianluv00@mi.fu-berlin.de>
2022-06-22 17:03:19 +0200

    Modified RequestTypes

--- Assets/Scrips/Agent/Request/RequestType.cs
+++ Assets/Scrips/Agent/Request/RequestType.cs
@@ -1,4 +1,4 @@
 ﻿
 public enum RequestType {
-	Help, Food, Healing, InformationGeneral, InformationSocial, InformationLocation, Wait, RequestAccepted
+	Help, Food, Healing, InformationSocial, InformationLocation
 }
