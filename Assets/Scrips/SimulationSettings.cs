31bba2561087e93c3e76c1ef0e65cfe73fcd0d9d
gianluv00 <gianluv00@mi.fu-berlin.de>
2022-07-01 17:53:33 +0200

    Removed unsused imports from Settings

--- Assets/Scrips/SimulationSettings.cs
+++ Assets/Scrips/SimulationSettings.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using Unity.VisualScripting;
-using UnityEngine;
 
 public class SimulationSettings {
     public static readonly int AgentViewDistance = 4;
