8a41dcfc588f5c88051f410f69fa12f27e4401fc
gianluv00 <gianluv00@mi.fu-berlin.de>
2022-07-15 17:32:47 +0200

    Updated EventHistoryManager to not successively update the event string but generate it on demand from a list of strings which resulted in a performace boost

--- Assets/Scrips/Agent/AgentEventHistoryManager.cs
+++ Assets/Scrips/Agent/AgentEventHistoryManager.cs
@@ -1,25 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Scrips.Agent {
 	public class AgentEventHistoryManager {
 		private string _name;
 		private List<string> _listOfEvents;
 
-		private string _totalStringOfEvents;
-
 		public AgentEventHistoryManager(string name) {
 			_name = name;
 			
 			_listOfEvents = new List<string>();
-
-			_totalStringOfEvents = "";
 		}
 
 		public void AddHistoryEvent(string historyEvent) {
 			string stringToAdd = _name + ": " + historyEvent + "\n";
 			
 			_listOfEvents.Add(stringToAdd);
-			_totalStringOfEvents += stringToAdd;
 		}
 
 		public List<string> GetListOfEvents() {
@@ -27,14 +23,13 @@ namespace Scrips.Agent {
 		}
 
 		public string GetListOfEventsAsString() {
-			return _totalStringOfEvents;
+			return String.Join(String.Empty, _listOfEvents.ToArray());
 		}
 
 		public void Tick(int timeStep) {
 			string stringToAdd = "\n" + "Time Step: " + timeStep + "\n"
 			                     + "-----------------------" + "\n";
 			_listOfEvents.Add(stringToAdd);
-			_totalStringOfEvents += stringToAdd;
 		}
 	}
 }
